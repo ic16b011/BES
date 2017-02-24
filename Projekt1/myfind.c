775c0b1041685dedb91d351bd4d9113e0cd37841
Ösi <a.oesi@gmx.at>
2017-02-24 10:03:45 +0100

    Add files via upload

--- Projekt1/myfind.c
+++ Projekt1/myfind.c
@@ -8,39 +8,76 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 void print(const char* argv[]);
+void do_dir(const char * dir_name, const char * const * parms);
+void do_file(const char * file_name, const char* argv[]);
 
 int main(int argc, const char* argv[])
 {
-	int i;
+	int i, j;
 
+	const char* parms;
 	for(i = 0; i < argc; i++)
 	{
-		printf("eingabe: %s\n",argv[i]);
-		if(!strcmp(argv[i], "-print"))
-			print(argv);
+		if(!strncmp(argv[i], "-", 1))
+		{
+			parms = &argv[i];
+			break;
+		}
+	}
+	
+	j=1;
+	while(j<i)
+	{
+		do_dir(argv[j], parms);
+		j++;
 	}
 
 	return 0;
 }
 
-void print(const char* argv[])
+void do_dir(const char * dir_name, const char * const * parms)
 {
-	DIR *hdir;
+	DIR *directory;
 	struct dirent *entry;
-
-	hdir = opendir(argv[1]);
-
+	
+	directory = opendir(dir_name);
+	//printf("%s\n", directory);
+	printf("%s\n%s\n", *parms, *(parms+1));
 	do
 	{
-		entry = readdir(hdir);
 
-		if(entry && !strcmp(entry->d_name, argv[2]))
+		entry = readdir(directory);
+
+		if(entry && strcmp(entry->d_name, ".") && strcmp(entry->d_name, ".."))
 		{
-			printf("%s\n", entry->d_name);
+			//printf("%s/%s\n", dir_name, entry->d_name);
+			struct stat buffer;
+			stat(entry->d_name, &buffer);
+			//printf("\nInode: %lu\n", buffer.st_ino);
+
+			do_file(entry->d_name, parms);
+			if(entry->d_type == DT_DIR)
+			{
+				//printf("Directory found!\n");
+				do_dir(entry->d_name, parms);
+			}
 		}
 	}while(entry);
 
-	closedir(hdir);
+	closedir(directory);
+}
+
+void do_file(const char * file_name, const char* argv[])
+{
+	struct stat buffer;
+	int status = stat(file_name, &buffer);
+	//printf("\nInode: %lu\n", buffer.st_ino);
+}
+
+void print(const char* argv[])
+{
+	
 }
