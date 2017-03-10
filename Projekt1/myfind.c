b5c0e6e93140604ca1fcf2899f72fe4205f4be68
Ösi <a.oesi@gmx.at>
2017-03-10 18:06:12 +0100

    Add files via upload

--- Projekt1/myfind.c
+++ Projekt1/myfind.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <libgen.h>
 #include <stdlib.h>
 #include <pwd.h>
+#include <time.h>
 
 void do_dir(const char* dir_name, const char* parms[]);
 void do_file(const char* file_name, const char* parms[]);
@@ -13,7 +15,7 @@ int main(int argc, const char* argv[])
 {
 	int i, j;
 
-	const char** parms;
+	const char** parms = NULL;
 
 	if(argc == 1) printf("Please use some parameters\n");
 
@@ -25,13 +27,6 @@ int main(int argc, const char* argv[])
 			break;
 		}
 	}
-
-	/*int z=0;
-	while(parms[z]!='\0')
-	{
-		printf("argv[%d]: %s\n", z, parms[z]);
-		z++;
-	}*/
 
 	j=1;
 	while(j<i)
@@ -52,68 +47,92 @@ void do_dir(const char* dir_name, const char* parms[])
 	char localname[100];
 	localname[0] = '\0';
 	int len;
+	int count;
 	
-	/* Öffne Directory */
-	directory = opendir(dir_name);
-	if(!directory)
-	{
-		printf("find: `%s': No such file or directory\n", dir_name);
-		return;
-	}
-
-	printf("%s\n", dir_name);
-	
-	do
-	{
-		/* Lese Directory Einträge */
-		entry = readdir(directory);
-
-		if(entry && strcmp(entry->d_name, ".") && strcmp(entry->d_name, ".."))
-		{
-			/* Erzeuge Fullpath */
-			strcpy(localname, dir_name);
-			len = strlen(localname);
-			if(localname[len-1] != '/')
-				strcat(localname, "/");
-			
-			strcat(localname, entry->d_name);
-
-			/* Wenn Directory, rufe do_dir rekursiv auf */
-			if(entry->d_type == DT_DIR)
-			{
-				do_dir(localname, parms);
-			}
-			
-			/* Wenn Regular File, rufe do_file auf */
-			else if(entry->d_type == DT_REG)
-			{
-				do_file(localname, parms);
-			}
-			
-			/* Typ unbekannt, versuche Typ über stat() herauszufinden */
-			else if(entry->d_type == DT_UNKNOWN)
-			{
-				status = stat(localname, &buffer);
+	count = 0;
+	status = stat(dir_name, &buffer);
 				
-				if(status != 0 && S_ISDIR(buffer.st_mode))
+	if(status == 0 && S_ISDIR(buffer.st_mode))
+	{
+		/* Öffne Directory */
+		directory = opendir(dir_name);
+		if(!directory)
+		{
+			printf("find: `%s': No such file or directory\n", dir_name);
+			return;
+		}
+		
+		/* keine Actions vorhanden */
+		if(parms == NULL)
+		{
+			printf("%s\n", dir_name);
+			
+			do
+			{
+				/* Lese Directory Einträge */
+				entry = readdir(directory);
+
+				if(entry && strcmp(entry->d_name, ".") && strcmp(entry->d_name, ".."))
 				{
-					do_dir(localname, parms);
+					/* Erzeuge Fullpath */
+					strcpy(localname, dir_name);
+					len = strlen(localname);
+					if(localname[len-1] != '/')
+						strcat(localname, "/");
+					
+					strcat(localname, entry->d_name);
+
+					/* Wenn Directory, rufe do_dir rekursiv auf */
+					if(entry->d_type == DT_DIR)
+					{
+						do_dir(localname, parms);
+					}
+					
+					/* Wenn Regular File, rufe do_file auf */
+					else if(entry->d_type == DT_REG)
+					{
+						do_file(localname, parms);
+					}
+					
+					/* Typ unbekannt, versuche Typ über stat() herauszufinden */
+					else if(entry->d_type == DT_UNKNOWN)
+					{
+						status = stat(localname, &buffer);
+						
+						if(status == 0 && S_ISDIR(buffer.st_mode))
+						{
+							do_dir(localname, parms);
+						}
+						else if(status == 0 && S_ISREG(buffer.st_mode))
+						{
+							do_file(localname, parms);
+						}
+						else
+						{
+							printf("Cannot read Entry!\n");
+							return;
+						}
+					}
 				}
-				else if(status != 0 && S_ISREG(buffer.st_mode))
-				{
-					do_file(localname, parms);
-				}
-				else
-				{
-					printf("Cannot read Entry!\n");
-					return;
-				}
+			}while(entry);
+		}
+		/* Actions vorhanden */
+		else
+		{
+			while(parms[count] != '\0')
+			{
+				
 			}
 		}
-	}while(entry);
-
-	/* Schließe Directory */
-	closedir(directory);
+		/* Schließe Directory */
+		closedir(directory);
+	}
+	else
+	{
+		do_file(dir_name, parms);
+	}
+	
+	return;
 }
 
 void do_file(const char* file_name, const char* parms[])
@@ -122,44 +141,94 @@ void do_file(const char* file_name, const char* parms[])
 
 	struct stat buffer;
 	int status = stat(file_name, &buffer);
-	/*printf("2: %s\n", file_name);
-	printf("Inode2: %lu\n\n", buffer.st_ino);*/
-
-	int count = 0;
-
-	while(parms[count] != '\0')
+	if(status == 0)
 	{
+		/*printf("2: %s\n", file_name);
+		printf("Inode2: %lu\n\n", buffer.st_ino);*/
 
-	if (!(strcmp(parms[count], "-print"))) printf("%s\n", file_name);
+		int count = 0;
 
-    else if (!(strcmp(parms[count], "-name")))
-    {
+		if(parms != NULL)
+		{
+			while(parms[count] != '\0')
+			{
 
-        if (!(strcmp(basename(file_name), parms[count+1]))) printf("%s\n", file_name);
-    }
+				if (!(strcmp(parms[count], "-print"))) 
+				{
+					printf("%s\n", file_name);
+					break;
+				}
 
-    else if (!(strcmp(parms[count], "-user")))
-    {
+				else if (!(strcmp(parms[count], "-name")))
+				{
+
+					if (!(strcmp(basename((char*)file_name), parms[count+1]))) printf("%s\n", file_name);
+					count += 2;
+				}
+
+				else if (!(strcmp(parms[count], "-user")))
+				{
+					if (buffer.st_uid == atoi(parms[count+1])) printf("%s\n", file_name);
+
+					struct passwd *p;
+
+					//if((p = getpwnam(parms[count+1])) == NULL) perror("User nicht vorhanden");//hier sollte man eine einmalige Fehlermeldung anzeigen
+
+					if(p->pw_uid == buffer.st_uid) printf("%s\n", file_name);
+					
+					count += 2;
+
+				}
+
+				else if (!(strcmp(parms[count], "-ls")))
+				{
+					printf("%9lu    %3lu ", buffer.st_ino, (buffer.st_blocks/2));
+
+					printf( (S_ISDIR(buffer.st_mode)) ? "d" : "-");
+					printf( (buffer.st_mode & S_IRUSR) ? "r" : "-");
+					printf( (buffer.st_mode & S_IWUSR) ? "w" : "-");
+					printf( (buffer.st_mode & S_IXUSR) ? "x" : "-");
+					printf( (buffer.st_mode & S_IRGRP) ? "r" : "-");
+					printf( (buffer.st_mode & S_IWGRP) ? "w" : "-");
+					printf( (buffer.st_mode & S_IXGRP) ? "x" : "-");
+					printf( (buffer.st_mode & S_IROTH) ? "r" : "-");
+					printf( (buffer.st_mode & S_IWOTH) ? "w" : "-");
+					printf( (buffer.st_mode & S_IXOTH) ? "x" : "-");
+
+					printf("    %lu", buffer.st_nlink);
+
+					struct passwd *p;
+					if ((p = getpwuid(buffer.st_uid)) != NULL)
+					{
+					printf("  %s", p->pw_name);
+					}
+					if ((p = getpwuid(buffer.st_gid)) != NULL)
+					{
+					printf(" %s", p->pw_name);
+					}
+					//time
 
 
-        if (buffer.st_uid == atoi(parms[count+1])) printf("%s\n", file_name);
+					struct tm *info;
+					char tm_buffer[200];
+					info = localtime(&(buffer.st_mtim));
 
-        struct passwd *p;
+					strftime(tm_buffer, 200, "%B %d %H:%M", info);
+					printf("  %s", tm_buffer);
 
-        if((p = getpwnam(parms[count+1])) == NULL) perror("User nicht vorhanden");//hier sollte man eine einmalige Fehlermeldung anzeigen
-        else
-        {
-        if(p->pw_uid == buffer.st_uid) printf("%s\n", file_name);
-        }
+					//printf("  %s", ctime(&(buffer.st_mtim)));
 
+					printf("   %s\n", file_name);
 
-    }
-
-
-
-	count++;
+					count++;
+				}
+			}
+		}
+		else
+			printf("%s\n", file_name);
 	}
+	else
+		printf("Errorrrrrrr!!\n\n");
+	
 	return;
-
-
 }
