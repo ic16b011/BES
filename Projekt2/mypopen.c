877933afcb545b8d946acd9d6bb0efb4ef0dc532
davidsattler <david.sattler.biz@gmail.com>
2017-04-22 22:42:03 +0200

    17/21 OK

--- Projekt2/mypopen.c
+++ Projekt2/mypopen.c
@@ -8,13 +8,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "mypopen.h"
+//global var
+static pid_t child_pid = -1;
 
 FILE *mypopen(const char *command, const char *type)
-{
+{	
 	int pipefd[2];
 	pid_t cpid;
 	int status;
-
+	
 	/* check if child-process already exists */
 	if (waitpid(0, &status, WNOHANG) != -1)
 	{
@@ -38,7 +40,9 @@ FILE *mypopen(const char *command, const char *type)
 	/* fork child process */
 	cpid = fork();
 	if (cpid == -1)
-	{
+	{	//notwendig für den Testfall 11
+		close(pipefd[0]);
+		close(pipefd[1]);
 		return NULL;
 	}
 
@@ -47,76 +51,65 @@ FILE *mypopen(const char *command, const char *type)
 	{
 		if (*type == 'w')
 		{
-			/* close read fd, because it is not used */
-			if (close(pipefd[0]) == -1)
+			/* close write fd, because the parent process needs to write something and the child needs to read it */
+			if (close(pipefd[1]) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
-			/* associate fd with stdout */
-			if (dup2(STDOUT_FILENO, pipefd[1]) == -1)
+			/* associate fd with stin */
+			if (dup2(pipefd[0], STDIN_FILENO) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
 			/* execute command */
-			if (execv("/bin/sh", (char* const*)command) == -1)
+			if (execl("/bin/sh", "sh", "-c", (const char *)command, (char *) NULL) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
-			if (close(pipefd[1]) == -1)
+			if (close(pipefd[0]) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 			/* return filestream */
-			_exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		}
 
 		else
 		{
-			/* close write fd, because it is not used */
-			if (close(pipefd[1]) == -1)
+			/* close read fd, because parent process wants to read the things the child process writes */
+			if (close(pipefd[0]) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
 			/* associate fd with stdout */
-			if (dup2(STDIN_FILENO, pipefd[0]) == -1)
+			if (dup2(pipefd[1], STDOUT_FILENO) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
 			/* execute command */
-			if (execv("/bin/sh", (char* const*)command) == -1)
+			if (execl("/bin/sh", "sh", "-c",(const char *)command, (char *) NULL) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 
-			if (close(pipefd[0]) == -1)
+			if (close(pipefd[1]) == -1)
 			{
 				exit(EXIT_FAILURE);
 			}
 			/* return filestream */
-			_exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		}
 	}
 	else
 	{
-		/* Elternprozess liest von Pipe */
+		child_pid = cpid;
+		/* Elternprozess schreibt auf die Pipe */
 		if (*type == 'w')
-		{
-			/* close write fd, because it is not used */
-			if (close(pipefd[1]) == -1)
-			{
-				return NULL;
-			}
-			/* return filestream */
-			return fdopen(pipefd[0], "r");
-		}
-
-		/* Elternprozess schreibt auf Pipe */
-		else
 		{
 			/* close read fd, because it is not used */
 			if (close(pipefd[0]) == -1)
@@ -126,41 +119,59 @@ FILE *mypopen(const char *command, const char *type)
 			/* return filestream */
 			return fdopen(pipefd[1], "w");
 		}
+
+		/* Elternprozess liest von der Pipe */
+		else
+		{
+			/* close write fd, because it is not used */
+			if (close(pipefd[1]) == -1)
+			{
+				return NULL;
+			}
+			/* return filestream */
+			return fdopen(pipefd[0], "r");
+		}
 	}
 }
 
 int mypclose(FILE *stream)
 {
 	int status;
-
-	/* check if child-process exists */
-	if (waitpid(0, &status, WNOHANG) == -1)
-	{
-		errno = ECHILD;
+	pid_t w = 0;
+	if(child_pid == -1) 
+		{
+		errno = ECHILD;		
 		return -1;
-	}
-	
-	/* ungültiger File Stream */
+		}
 	if (stream == NULL || fgetc(stream) == '\0')
 	{
 		errno = EINVAL;
 		return -1;
 	}
-
-	/* wait for child-process with same group id to terminate */
-	do
-	{
-		if (waitpid(0, &status, WNOHANG) == -1)
-		{
-			return -1;
-		}
-	} while (WIFEXITED(status) == 0);
-
-	/* close filestream */
+	
 	if (fclose(stream) != 0)
 	{
-		return NULL;
+		return -1;
 	}
-
+	
+	while(w != child_pid){
+		w = waitpid(child_pid, &status, WUNTRACED | WCONTINUED);
+    if (w == -1) {
+		printf("fehler bei waitpid im mypclose\n");
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+	}
+	
+	if (WIFEXITED(status)) 
+	{
+		status = WEXITSTATUS(status);
+	}
+	else
+	{
+		errno = ECHILD;
+		return -1;
+	}
+	
 	return status;
 }
